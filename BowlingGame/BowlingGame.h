4fe379bca13edc3c8e3fea6cdcd73b6db85763b9
Alex Cristea <alexcristea@me.com>
2015-01-11 15:31:38 +0200

    Initial Commit

---
+++ BowlingGame/BowlingGame.h
@@ -0,0 +1,19 @@
+//
+//  BowlingGame.h
+//  BowlingGame
+//
+//  Created by Alex Cristea on 11/01/15.
+//  Copyright (c) 2015 Alex Cristea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BowlingGame.
+FOUNDATION_EXPORT double BowlingGameVersionNumber;
+
+//! Project version string for BowlingGame.
+FOUNDATION_EXPORT const unsigned char BowlingGameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BowlingGame/PublicHeader.h>
+
+
